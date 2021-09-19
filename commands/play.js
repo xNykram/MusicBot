ea0b14b1e163fe647e3beee95c8da34e117a50d8
Mateusz Burdyna <mateuszburdyna@mateuszs-macbook-air.home>
2021-09-19 15:07:40 +0200

    added queue handling

--- commands/play.js
+++ commands/play.js
@@ -1,14 +1,19 @@
 const join = require('./join.js');
+const leave = require('./leave.js');
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 const voice = require('@discordjs/voice');
 
 var connection = null;
 var audioPlayer = null;
+var subscribe = null;
+const queues = new Map();
+
 
 module.exports = {
     name: 'play',
     description: 'Search and plays a song',
+    queues: queues,
     async execute(message, args){
         if(args.length == 0)
         {
@@ -26,29 +31,72 @@ module.exports = {
         const query = args.join(' ');
         message.reply(query);
         const video = await videoFinder(query);
-        
+
+        if(!connection)
+        {
+            join.execute(message, args);
+            connection = voice.getVoiceConnection(message.guild.id);
+        }
 
         if(video)
         {
-            message.reply(`Playing ${video.title} (${video.duration.timestamp})`);
-            if(!connection)
+            var serverQueue = queues.get(message.guild.id);
+            if(!serverQueue)
             {
-                join.execute(message, args);
+                serverQueue = [];
+                queues.set(message.guild.id, serverQueue);
             }
-            if(!audioPlayer)
+            serverQueue.push(video);
+            if(serverQueue.length == 1)
             {
-                audioPlayer = voice.createAudioPlayer();    
+                message.reply(`Playing ${video.title} (${video.duration.timestamp})`);
             }
-            connection = voice.getVoiceConnection(message.guild.id);
-            const stream = ytdl(video.url, {filter: 'audioonly'});
-            const resource = voice.createAudioResource(stream);
-            audioPlayer.play(resource);
-            const subscribe = connection.subscribe(audioPlayer);
+            else
+            {
+                pos = serverQueue.length;
+                message.reply(
+                    `Added ${video.title} (${video.duration.timestamp}) to queue (pos: ${pos});`
+                )
+            }
+            play(message.guild, serverQueue);
         }
         else
         {
             message.reply("Couldn't find requested query.");
         }
-        
     }
+}
+
+async function play(guild, queue){
+    if(!connection)
+        return false;
+    if(!audioPlayer)
+        audioPlayer = voice.createAudioPlayer();    
+    if(!subscribe)
+        subscribe = connection.subscribe(audioPlayer);
+    if(subscribe.player.state.status !== 'idle')
+    {
+        return false;
+    }
+    song = queue.shift();
+    const stream = ytdl(song.url, {filter: 'audioonly'});
+    const resource = voice.createAudioResource(stream);
+    audioPlayer.play(resource);
+    subscribe.player.on("stateChange", (oldState, newState) => {
+        console.log(`OldState: ${oldState.status}, NewState: ${newState.status}`);
+        if(newState.status === 'idle')
+        {
+            if(queue.length == 0)
+            {
+                connection.disconnect();
+                connection.destroy();
+                subscribe.unsubscribe();
+                connection = null;
+            }
+            else
+            {
+                play(guild, queue);
+            }
+        }
+    });
 }

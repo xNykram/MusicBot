06b0d2271cd13b25edfa285781fc4e7ea498ea8e
xNykram <nykram@wp.pl>
2022-04-11 21:10:40 +0200

    - implement logMessage func

--- app/src/tools.ts
+++ app/src/tools.ts
@@ -1,22 +1,6 @@
 import { Message } from 'discord.js'
-import { Logs } from './db/models/logs';
 
 export function isInVoice(message: Message) {
     return message.member?.voice?.channel != null
 }
 
-export async function logMessage(server: string, serverID: string, taskType: string, error: unknown, status: boolean) {
-    try{
-        const date: Date = new Date();
-        const log = await Logs.create({
-            server_name: server,
-            server_id: serverID,
-            task: taskType,
-            message: error,
-            failed: status,
-        })
-    }
-    catch(error) {
-        console.log(error)
-    }
-}
